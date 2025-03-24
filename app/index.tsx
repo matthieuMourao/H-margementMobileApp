2beb363d2af6e361916649575961850890dc4b2b
matthieuMourao <matthieumourao9@gmail.com>
2025-03-24 21:50:12 +0100

    modification du text

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hémargement</Text>
     </View>
   );
 }
