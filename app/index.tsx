d682b42ae27e3b424938a226cfc2f0d0e50bd3b8
matthieuMourao <matthieumourao9@gmail.com>
2025-03-25 14:01:10 +0100

    modification du nom

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image} from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hémargement</Text>
+      <image></image>
+      <Text>Connexion</Text>
     </View>
   );
 }
